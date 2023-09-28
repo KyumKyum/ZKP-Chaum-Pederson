5a3f3dd3279266865000e6971400ab13623ba59d
KyumKyum <mycheesepasta@gmail.com>
2023-09-28 18:11:49 +0900

    1024 bit example

--- src/lib.rs
+++ src/lib.rs
@@ -3,77 +3,154 @@
 // Protocol: Chaum-Pedersen Protocol
 
 // Require following functions
-// - exponentiate(): a^x mod p
+// - exponentiate(): a^x mod P
 // - solve(): generate s
 // - verify(): verify s
+
 
 // Require following module
 // - Random Generator
 // - Big Integers
-use num_bigint::{BigUint, RandBigInt};
-use rand;
 
-pub struct ZKP {
-    p: BigUint,
-    q: BigUint,
-    alpha: BigUint,
-    beta: BigUint,
-}
 
-impl ZKP {
-    //* pow
-    // a^x mod b
-    // output = n^exp mod modulus
-    pub fn pow(n: &BigUint, exp: &BigUint, modulus: &BigUint) -> BigUint {
-        n.modpow(exp, modulus)
+mod constant;
+
+//* module zkp chaum-pedersen
+mod zkp_cp {
+    use num_bigint::{BigUint, RandBigInt};
+    use rand;
+    pub struct ZKP {
+        pub(crate) p: BigUint,
+        pub(crate) q: BigUint,
+        pub(crate) alpha: BigUint,
+        pub(crate) beta: BigUint,
     }
 
-    //* solve
-    // output = s = k - c * x mod q
-    pub fn solve(&self, k: &BigUint, c: &BigUint, x: &BigUint) -> BigUint {
-        //* If k is less than c*x, it will occur downflow.
-        return if *k >= c * x {
-            (k - c * x).modpow(&BigUint::from(1u32), &self.q)
-        } else {
-            &self.q - (c * x - k).modpow(&BigUint::from(1u32), &self.q)
+    impl ZKP {
+        //* pow
+        // a^x mod b
+        // output = n^exp mod modulus
+        pub fn pow(n: &BigUint, exp: &BigUint, modulus: &BigUint) -> BigUint {
+            n.modpow(exp, modulus)
         }
-    }
 
-    //* Verify
+        //* solve
+        // output = s = k - c * x mod q
+        pub fn solve(&self, k: &BigUint, c: &BigUint, x: &BigUint) -> BigUint {
+            //* If k is less than c*x, it will occur downflow.
+            return if *k >= c * x {
+                (k - c * x).modpow(&BigUint::from(1u32), &self.q)
+            } else {
+                &self.q - (c * x - k).modpow(&BigUint::from(1u32), &self.q)
+            }
+        }
+
+        //* Verify
 // cond1: r1 = a^s * y1^c
 // cond2: r2 - b^s * y2^c
-    pub fn verify(
-        &self,
-        r1: &BigUint,
-        r2: &BigUint,
-        y1: &BigUint,
-        y2: &BigUint,
-        c: &BigUint,
-        s: &BigUint,
-    ) -> bool {
-        let cond1: bool = *r1 == (&self.alpha.modpow(s, &self.p) * y1.modpow(c, &self.p))
-            .modpow(&BigUint::from(1u32), &self.p);
+        pub fn verify(
+            &self,
+            r1: &BigUint,
+            r2: &BigUint,
+            y1: &BigUint,
+            y2: &BigUint,
+            c: &BigUint,
+            s: &BigUint,
+        ) -> bool {
+            let cond1: bool = *r1 == (&self.alpha.modpow(s, &self.p) * y1.modpow(c, &self.p))
+                .modpow(&BigUint::from(1u32), &self.p);
 
-        let cond2: bool = *r2 == (&self.beta.modpow(s, &self.p) * y2.modpow(c, &self.p))
-            .modpow(&BigUint::from(1u32), &self.p);
+            let cond2: bool = *r2 == (&self.beta.modpow(s, &self.p) * y2.modpow(c, &self.p))
+                .modpow(&BigUint::from(1u32), &self.p);
 
-        cond1 && cond2
-    }
+            cond1 && cond2
+        }
 
-    pub fn gen_rand(limit: &BigUint) -> BigUint {
-        let mut rng = rand::thread_rng(); //* Generate random number
+        pub fn gen_rand(limit: &BigUint) -> BigUint {
+            let mut rng = rand::thread_rng(); //* Generate random number
 
-        rng.gen_biguint_below(limit)
+            rng.gen_biguint_below(limit)
+        }
     }
 }
 
+
+//* TEST
+//==========================
 
 #[cfg(test)]
 mod test {
     use super::*; //* import all
+    use num_bigint::{BigUint, RandBigInt};
+    use rand;
+    use zkp_cp::ZKP;
+    use constant;
+    #[test]
+    fn test_1024bits() {
+        //* Init
+        let (P,G,Q) = constant::gen_large_prime();
+
+        let p: BigUint = BigUint::from_bytes_be(&P);
+        let q: BigUint = BigUint::from_bytes_be(&Q);
+
+        //* Public
+        let alpha: BigUint = BigUint::from_bytes_be(&G);
+        //* alpha^x is also will be a generator -> define as an beta
+        let beta: BigUint = alpha.modpow(&ZKP::gen_rand(&q), &p);
+
+        //* ZKP Protocol Structure
+        let zkp:ZKP = ZKP{
+            p: p.clone(),
+            q: q.clone(),
+            alpha: alpha.clone(),
+            beta: beta.clone()
+        };
+
+        //* Secrets
+        let x: BigUint = ZKP::gen_rand(&q);
+        let k: BigUint = ZKP::gen_rand(&q);
+
+        //* Challenge
+        let c: BigUint = ZKP::gen_rand(&q);
+
+        //* Prover
+        let y1: BigUint = ZKP::pow(&alpha, &x, &p); //* This will be computed as 2
+        let y2: BigUint = ZKP::pow(&beta, &x, &p); //* This will be computed as 3
+
+        //* Verifier
+        let r1: BigUint = ZKP::pow(&alpha, &k, &p); //* This will be computed as 8
+        let r2: BigUint = ZKP::pow(&beta, &k, &p); //* This will be computed as 4
+
+        //* Solve
+        let s: BigUint = zkp.solve(&k, &c, &x); //* This will be computed as 5
+
+        //* Assertion
+        //* Verify
+        let res: bool = zkp.verify(&r1, &r2, &y1, &y2, &c, &s); //* This will be computed as true.
+
+        //* Assertion
+        assert!(res); //* Assert to be true
+
+        //* Eavesdropped key
+        let eaves_x: BigUint = BigUint::from(7u32);
+        let eaves_s: BigUint = zkp.solve(&k, &c, &eaves_x);
+        let eaves_res: bool = zkp.verify(&r1, &r2, &y1, &y2, &c, &eaves_s); //* This will be computed as false.
+
+        //* Assertion
+        assert!(!eaves_res);
+    }
 
     #[test]
-    fn test_example() {
+    fn test_rand(){
+        let rand1: BigUint = ZKP::gen_rand(&BigUint::from(100u32));
+        let rand2: BigUint = ZKP::gen_rand(&BigUint::from(100u32));
+
+        println!("{}", rand1);
+        println!("{}", rand2);
+    }
+
+    #[test]
+    fn test_example(){
         //* Public
         let alpha: BigUint = BigUint::from(4u32);
         let beta: BigUint = BigUint::from(9u32);
@@ -130,14 +207,5 @@ mod test {
 
         //* Assertion
         assert!(!eaves_res);
-    }
-
-    #[test]
-    fn test_rand(){
-        let rand1: BigUint = ZKP::gen_rand(&BigUint::from(100u32));
-        let rand2: BigUint = ZKP::gen_rand(&BigUint::from(100u32));
-
-        println!("{}", rand1);
-        println!("{}", rand2);
     }
 }
